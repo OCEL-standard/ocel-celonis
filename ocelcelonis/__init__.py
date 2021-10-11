2a9bce11e589dd73a7cc2f578c804d86ded9df03
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-10-11 07:21:09 +0200

    first commit
    first commit

---
+++ ocelcelonis/__init__.py
@@ -0,0 +1,8 @@
+from ocelcelonis import principal
+
+__version__ = '0.0.1'
+__doc__ = "OCEL support for Celonis"
+__author__ = 'PADS'
+__author_email__ = 'a.berti@pads.rwth-aachen.de'
+__maintainer__ = 'PADS'
+__maintainer_email__ = "a.berti@pads.rwth-aachen.de"
---
+++ setup.py
@@ -0,0 +1,29 @@
+from os.path import dirname, join
+
+import ocelcelonis
+from setuptools import setup
+
+
+def read_file(filename):
+    with open(join(dirname(__file__), filename)) as f:
+        return f.read()
+
+
+setup(
+    name="ocel-celonis",
+    version=ocelcelonis.__version__,
+    description=ocelcelonis.__doc__.strip(),
+    long_description=read_file('README.md'),
+    author=ocelcelonis.__author__,
+    author_email=ocelcelonis.__author_email__,
+    py_modules=[ocelcelonis.__name__],
+    include_package_data=True,
+    packages=['ocelcelonis'],
+    url='http://www.pm4py.org',
+    license='Apache 2.0',
+    install_requires=[
+        "ocel",
+        "pandas",
+        "pycelonis"
+    ]
+)
